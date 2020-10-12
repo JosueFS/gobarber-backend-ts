78ef0f0eadca4558099fb6fa45afa72718d97c32
JosueFS <josue.18rj@gmail.com>
2020-10-11 21:17:58 -0300

    F04_17: Create User Tests ✅

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,40 @@
+import { uuid } from 'uuidv4';
+
+import IUserRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+class UsersRepository implements IUserRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.id === id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(u => u.email === email);
+
+    return user;
+  }
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    await this.save(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    this.users.push(user);
+
+    return user;
+  }
+}
+
+export default UsersRepository;

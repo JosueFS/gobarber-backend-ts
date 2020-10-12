566553d95feeb9870225425ec2a7d8b21b6898e8
JosueFS <josue.18rj@gmail.com>
2020-10-12 00:54:01 -0300

    F04_18: Authentication Tests ✅

---
+++ src/modules/users/providers/HashProvider/implementations/BCryptyHashProvider.ts
@@ -0,0 +1,14 @@
+import { hash, compare } from 'bcryptjs';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
+
+export default class BCryptHashProvider implements IHashProvider {
+  public async generateHash(payload: string): Promise<string> {
+    return hash(payload, 8);
+  }
+
+  public async compareHash(payload: string, hashed: string): Promise<boolean> {
+    const passwordMatch = await compare(hashed, payload);
+
+    return passwordMatch;
+  }
+}

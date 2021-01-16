f8613f6fea025a82dc94df62a89681d901deacab
JosueFS <josue.18rj@gmail.com>
2021-01-16 00:35:31 -0300

    test: 100% coverage tests

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfileService', () => {
     expect(updatedUser.email).toBe('newjohn@example.com');
   });
 
+  it('should not be able to show the profile', async () => {
+    await expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user',
+        name: 'New John',
+        email: 'newjohn@example.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to email already used', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',

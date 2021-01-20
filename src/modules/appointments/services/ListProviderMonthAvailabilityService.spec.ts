e17dea11682bbf7ba44c5b75a82daac43ae4b126
JosueFS <josue.18rj@gmail.com>
2021-01-20 17:00:50 -0300

    test: Provider Day Availability test coverage

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -13,23 +13,21 @@ describe('ListProviderMonthAvailabilityService', () => {
   });
 
   it('should be able to list the month availability from provider', async () => {
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
-    });
+    const hours = Array.from({ length: 10 }, (_, i) => i + 8);
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
-    });
+    (async function loop() {
+      const all = hours.map(async h => {
+        await fakeAppointmentsRepository.create({
+          provider_id: 'user',
+          date: new Date(2020, 4, 20, h, 0, 0),
+        });
 
-    await fakeAppointmentsRepository.create({
-      provider_id: 'user',
-      date: new Date(2020, 4, 21, 8, 0, 0),
-    });
+        await Promise.all(all);
+      });
+    })();
 
     const availability = await listProviderMonthAvailability.execute({
-      user_id: 'user',
+      provider_id: 'user',
       year: 2020,
       month: 5,
     });
@@ -38,7 +36,7 @@ describe('ListProviderMonthAvailabilityService', () => {
       expect.arrayContaining([
         { day: 19, available: true },
         { day: 20, available: false },
-        { day: 21, available: false },
+        { day: 21, available: true },
         { day: 22, available: true },
       ]),
     );

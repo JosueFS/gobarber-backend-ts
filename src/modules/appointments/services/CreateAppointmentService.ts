0ba4a080090adae04002f9c9bb7be1b0dcdfb575
JosueFS <josue.18rj@gmail.com>
2021-01-21 21:48:34 -0300

    feature: user has been attached to appointment

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour } from 'date-fns';
+import { isAfter, startOfHour } from 'date-fns';
 import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
@@ -8,6 +8,7 @@ import IAppoitmentsRepository from '@modules/appointments/repositories/IAppointm
 
 interface IRequestDTO {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
@@ -16,10 +17,11 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppoitmentsRepository,
-  ) { }
+  ) {}
 
   public async execute({
     provider_id,
+    user_id,
     date,
   }: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
@@ -32,8 +34,13 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked.');
     }
 
+    if (isAfter(Date.now(), appointmentDate)) {
+      throw new AppError('This appointment not is possible');
+    }
+
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
